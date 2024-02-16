8a4f788e85849a9f66c480eacc70620a42a30e56
Croydon <git@cr0ydon.com>
2024-02-16 06:17:37 +0100

    Update SDL to 2.28.5

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class Sauerbraten(ConanFile):
 
     def requirements(self):
         self.requires("enet/1.3.17")
-        self.requires("sdl/2.28.3")
+        self.requires("sdl/2.28.5")
         self.requires("sdl_image/2.6.3")
         self.requires("sdl_mixer/2.0.4")
         self.requires("zlib/1.3")
