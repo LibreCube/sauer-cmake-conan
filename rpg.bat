c1c432d76a143a260e892e6a1c24eed7131a2341
Croydon <git@cr0ydon.com>
2024-02-16 04:44:55 +0100

    Build RPG

--- rpg.bat
+++ rpg.bat
@@ -1,1 +1,14 @@
-bin\rpg.exe -q"$HOME\My Games\GoSauer" -grpg-log.txt -r %*
+@ECHO OFF
+
+set SAUER_BIN=bin
+
+IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+    set SAUER_BIN=bin64
+)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+    set SAUER_BIN=bin64
+)
+
+set /p version=<version.txt
+
+start %SAUER_BIN%\rpg.exe "-q$HOME\My Games\GoSauer" -grgp-log.txt "-kpackages/sauerbraten/1.%version%" %*
